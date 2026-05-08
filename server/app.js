0187db8f41f740ae1296e632ce196793f8bd45f6
tanvireamon <eamon098765@gmail.com>
2026-05-08 22:25:04 +0600

    add database folder and README

--- server/app.js
+++ server/app.js
@@ -1,1 +1,9 @@
- 
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
- 
+const app = require('./app');
+const db = require('./config/db');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
